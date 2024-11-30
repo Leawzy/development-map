40b66c8dabc7cf5bc4825296a24d00b453dc7ecb
Iliya <facyoucraft@gmail.com>
2024-11-30 14:55:19 +0300

    feat(project): init project

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router';
+import Header from '@/components/layout/header/HeaderLayout.vue';
+import Footer from '@/components/layout/footer/FooterLayout.vue';
+</script>
+
+<template>
+  <Header />
+  <RouterView />
+  <Footer />
+</template>
+
+<style scoped></style>
